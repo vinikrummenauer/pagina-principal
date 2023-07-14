915dd246d706160b8e4ca91ef307928799a89e34
Vinicius Krummenauer <viniciuskdepaula@gmail.com>
2023-07-14 16:11:45 -0300

    principal

--- src/components/PaginaPrincipal.jsx
+++ src/components/PaginaPrincipal.jsx
@@ -6,12 +6,12 @@ import { useNavigate } from 'react-router-dom';
 
 const PaginaPrincipal = () => {
   return (
-    <div className="container3">
+    <div className={window.innerHeight > 768 ? "container4" : "container3"}>
   <div className="div1 text-black">
     <div className="image-container">
       <img src="../../assets/img/consepro11.png" alt="Exemplo de imagem" />
     </div>
-    <div className="content2 w-50">
+    <div className="content2 w-50 p-3">
       <h2>ESTACIONE COM CRÉDITO</h2>
       <p>Crie sua conta e estacione com crédito, clique no botão abaixo!</p>
       <a href="https://estacionamentorotativo.conseprotaquara.com.br">
@@ -27,7 +27,7 @@ const PaginaPrincipal = () => {
     </div>
   </div>
   <div className="div2 text-white">
-    <div className="content">
+    <div className="content p-3">
       <h2>ESTACIONE RÁPIDO</h2>
       <p>Realize um estacionamento de forma simples e rápida.</p>
       <a href="https://avulso.conseprotaquara.com.br">
@@ -49,7 +49,7 @@ const PaginaPrincipal = () => {
     <div className="image-container3">
       <img src="../../assets/img/whatsconsepro5.png" alt="Exemplo de imagem" />
     </div>
-    <div className="content2 w-50">
+    <div className="content2 w-50 p-3">
       <h2>ESTACIONE POR WHATS</h2>
       <p>Estacione utilizando o whatsapp, clique no botão abaixo e confira.</p>
       <a href="https://api.whatsapp.com/send?phone=5186604241&text=Ol%C3%A1!">
